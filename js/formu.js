d01709e5c59e6248a864e28e26bed033131a4d6e
Andrés Petronio <andpetronio@outlook.com>
2017-03-21 21:01:48 -0300

    actualizado el formulario de contacto

---
+++ js/formu.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+	
+	$(".form-contacto").bind("submit",function(){
+		
+		$.ajax({
+		type:$(this).attr("method"),
+		url:$(this).attr("action"),
+		data:$(this).serialize(),
+		beforeSend:function(){  
+            $('#enviando').css("display", "block");
+            },
+        complete:function(){
+        	$('#enviando').css("display","none");
+        	},
+       	success: function(){
+       		/*swal({
+  				title: "Formulario enviado:",
+  				text: "En breve nos pondremos en contacto con vos.",
+  				imageUrl: "thumbs-up.jpg"
+			});*/
+			swal({
+  				title: '<span class="informa_titulo">Formulado enviado</span>',
+  				imageUrl: 'img/thumbs-up.jpg',
+  				imageWidth: 300,
+  				imageHeight: 300,
+  				html: $('<div>')
+    				.addClass('informa_texto')
+    				.text('En breve nos pondremos en contacto con vos.'),
+  				animation: false,
+  				customClass: 'animated tada',
+  				buttonsStyling: false,
+  				confirmButtonClass: 'boton_confirma'
+				});
+			
+			$('#nombre').val('');
+			$('#email').val('');
+			$('#comentario').val('');
+			}
+		});
+
+		return false
+	});
+});
+
