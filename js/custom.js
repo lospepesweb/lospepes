90e005da09e18c636c24c4808ad185cc64b7a8c9
Lucho Peñafiel <lucianopenafiel@gmail.com>
2017-06-19 23:32:49 -0300

    arreglando detalles de diseño

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,56 @@
-(function (){
+// (function (){
 
 // -- SELECCIÓN DEL IDIOMA -- //
 
 // VARIABLES
-var esp = document.getElementById("esp");
-var eng = document.getElementById("eng");
-
+var btnEsp = document.getElementById("esp");
+var btnEng = document.getElementById("eng");
 
 // FUNCIONES
 // Agrego clase al botón español y se la saco al ingles
 var colorEsp = function(){
-	esp.className = "btn btn-languaje-active";
-	eng.className = "btn btn-languaje";
+	btnEsp.className = "btn btn-languaje-active";
+	btnEng.className = "btn btn-languaje";
 };
 
 // Agrego clase al botón ingles y se la saco al español
 var colorEng = function(){
-	eng.className = "btn btn-languaje-active";
-	esp.className = "btn btn-languaje";
+	btnEng.className = "btn btn-languaje-active";
+	btnEsp.className = "btn btn-languaje";
 };
 
+// EVENTOS
+btnEsp.addEventListener("click", colorEsp);
+btnEng.addEventListener("click", colorEng);
+
+
+
+// -- CENTRADO VERTICAL DEL FORMULARIO -- //
+
+// VARIABLES
+var alturaPantalla = window.innerHeight;
+var alturaDiv = 577 + 2 // document.getElementById("modal-dialog").clientHeight;
+var margen = (alturaPantalla - alturaDiv) / 2;
+
+// FUNCIONES
+function seteo(){
+	document.getElementById("modal-dialog").style.marginTop = margen+"px";
+	document.getElementById("modal-dialog").style.marginBottom = margen+"px";
+
+};
 
 // EVENTOS
-esp.addEventListener("click", colorEsp);
-eng.addEventListener("click", colorEng);
+seteo();
 
-}())
+
+
+
+// -- TRANSFORMACIÓN DEL MENÚ HAMBURGUESA -- //
+
+$(document).ready(function(){
+	$('#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+// }())
